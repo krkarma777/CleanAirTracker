8f558027ae66eeab68eee15da9ab31b77644f62e
krkarma777 <krkarma777@gmail.com>
2024-05-03 11:47:11 +0900

    AirQualityService 클래스 추가로 데이터 처리 로직 분리
    - AirQualityService 클래스를 생성하여 API 데이터 호출과 데이터베이스 저장 로직을 분리함. 이 클래스는 외부 API로부터 공기질 데이터를 가져오고, 새로운 데이터만 데이터베이스에 저장하는 기능을 담당함. 이를 통해 코드의 가독성과 유지보수성을 향상시키며, 라우터 로직을 간소화함

---
+++ src/servicies/AirQualityService.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { DataSource } from 'typeorm';
+import { AirQuality } from "../entity/AirQuality.entity";
+import { IAirQuality } from "../entity/IAirQuality";
+
+export class AirQualityService {
+    constructor(private dataSource: DataSource) {
+    }
+
+    async fetchAirQualityData(apiKey: string, sidoName: string) {
+        const url = `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${ apiKey }&returnType=json&numOfRows=100&pageNo=1&sidoName=${ encodeURIComponent(sidoName) }&ver=1.0`;
+        try {
+            return await axios.get(url);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error('Failed to fetch data');
+        }
+    }
+
+    async saveAirQualityData(items: IAirQuality[]) {
+        const airQualityRepository = this.dataSource.getRepository(AirQuality);
+        return Promise.all(items.map(async (item) => {
+            const existingData = await airQualityRepository.findOneBy({
+                dataTime: item.dataTime,
+                stationName: item.stationName
+            });
+
+            if (!existingData) {
+                const airQuality = airQualityRepository.create(item);
+                await airQualityRepository.save(airQuality);
+            }
+        }));
+    }
+}
