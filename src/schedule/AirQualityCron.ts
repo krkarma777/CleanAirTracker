d92d23d1c968bacb47e55f3953417bb043f283e9
krkarma777 <krkarma777@gmail.com>
2024-05-04 23:01:33 +0900

    크론 작업을 위한 AirQualityCron 클래스 추가
    - AirQualityService를 사용하여 매 분마다 공기질 데이터를 수집하고 저장하는 크론 작업을 관리하는 AirQualityCron 클래스를 구현했습니다. 이 클래스는 매 분마다 실행되며, 서울의 공기질 데이터를 처리합니다.

---
+++ src/schedule/AirQualityCron.ts
@@ -0,0 +1,34 @@
+// AirQualityCron.ts
+import cron from 'node-cron';
+import { AirQualityService } from '../servicies/AirQualityService';
+
+export class AirQualityCron {
+    private airQualityService: AirQualityService;
+
+    constructor(airQualityService: AirQualityService) {
+        this.airQualityService = airQualityService;
+    }
+
+    public startCronJob(): void {
+        // 매 10분마다 실행되는 크론 작업
+        cron.schedule('*/10 * * * *', async () => {
+            const apiKey = process.env.API_KEY;
+            if (!apiKey) {
+                console.error('API Key is undefined');
+                return;
+            }
+
+            const sidoNames = ['서울']; // 시도 이름들 추가 예정
+            for (const sidoName of sidoNames) {
+                try {
+                    const response = await this.airQualityService.fetchAirQualityData(apiKey, sidoName);
+                    const items = response.data.response.body.items;
+                    await this.airQualityService.saveAirQualityData(items);
+                    console.log(`Data for ${sidoName} has been successfully processed.`);
+                } catch (error) {
+                    console.error(`Error saving data for ${sidoName}:`, error);
+                }
+            }
+        });
+    }
+}
