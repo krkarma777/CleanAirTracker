e31d95e1f78287ada2eb1eabe9f7ff684ba988f1
krkarma777 <krkarma777@gmail.com>
2024-05-07 00:24:01 +0900

    Adjust styles for improved layout in Clean Air Tracker
    - Removed the max-width limit from the main tag for a more fluid layout, allowing the content to better adjust across various screen sizes.
    - Added a media query to hide the header (h1) on devices with widths of 480px or less to simplify the interface on smaller screens.

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,35 @@
 <script lang="ts">
-	import AirQuality from './AirQuality.svelte';
+    import AirQuality from './AirQuality.svelte';
 </script>
 
 <main>
-	<h1>Welcome to Clean Air Tracker</h1>
-	<AirQuality />
+    <h1>Welcome to Clean Air Tracker</h1>
+    <AirQuality/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+
+    @media (max-width: 480px) {
+        h1 {
+            display: none;
+        }
+    }
 </style>
